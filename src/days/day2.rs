96581f7b0c6b2a94656f9eb7c43f7f59b9425812
Jesse Williams <jesse@negativespacesounds.com>
2023-12-04 15:50:41 +1100

    --amend

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,3 +1,9 @@
+use crate::helpers::get_data_as_array;
+use std::process::exit;
+
 pub fn run() -> String {
+    let data = get_data_as_array("../day2example.json".to_string());
+    println!("{:?}", data);
+    exit(0);
     return String::from("Not started!");
 }
